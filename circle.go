fe97108979d8736e421ef3308090db7f71bf31c4
v.lozhkin <v.lozhkin@corp.mail.ru>
2021-12-16 20:29:39 +0300

    my awesome mode

---
+++ circle.go
@@ -0,0 +1,26 @@
+package cirlces
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	MyRadius int
+}
+
+func (c Circle) Square() float64 {
+	return math.Pi * float64(c.MyRadius*c.MyRadius)
+}
+
+func (c Circle) String() string {
+	return fmt.Sprintf("Круг с радиусом %d и площадью %.2f", c.Radius, c.Square())
+}
+
+func (c Circle) Radius() int {
+	return c.MyRadius
+}
+
+func (c Circle) Point() (int, int) {
+	return 0, 0
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/v-lozhkin/cirlces
+
+go 1.16
